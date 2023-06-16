223f4b62e26fc71b324e8b15d17062cf63d6cc4f
soonable@naver.com <soonable@naver.com>
2023-06-16 11:42:58 +0900

    Initial commit

---
+++ src/test/java/com/soon/thymeleafbasic/ThymeleafBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soon.thymeleafbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
